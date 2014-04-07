35c82e292aab1d347f6d2742a5a540fad881e2e7
dragon <dragon7780@hotmail.nl>
2014-04-07 13:56:43 +0200

    Improvements from linaro
    Signed-off-by: dragon <dragon7780@hotmail.nl>
    
    Conflicts:
    	ext4_utils/Android.mk

--- ext4_utils/Android.mk
+++ ext4_utils/Android.mk
@@ -114,6 +114,24 @@ LOCAL_SHARED_LIBRARIES := \
     libz
 include $(BUILD_EXECUTABLE)
 
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := make_ext4fs_main.c
+LOCAL_MODULE := utility_make_ext4fs
+LOCAL_MODULE_STEM := make_ext4fs
+LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
+LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
+LOCAL_STATIC_LIBRARIES += \
+    libext4_utils_static \
+    libselinux \
+    libsparse_static \
+    libz \
+    libcutils \
+    libc
+
+include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := ext2simg.c
